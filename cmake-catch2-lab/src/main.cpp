ee084a52a1ca1d7a7eb594d09c1ee3da9535baa0
Rabbit <166239200+RabbitAsakura@users.noreply.github.com>
2025-08-25 23:57:26 +0000

    math_functions.h

---
+++ cmake-catch2-lab/src/main.cpp
@@ -0,0 +1,10 @@
+#include "math_functions.h"
+#include <iostream>
+
+int main()
+{
+    double num = 25.0;
+    double result = compute_sqr_root(num);
+    std::cout << "Square root of "<< num << "is " << result << std::endl;
+    return 0;
+}
