d87d3522cc3f5172777ea972e510062f5834c359
Rabbit <166239200+RabbitAsakura@users.noreply.github.com>
2025-09-03 01:47:41 +0000

    math_test.cpp

---
+++ tests/math_test.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_test_macros.hpp>
+#include "math_functions.h"
+
+TEST_CASE("Square root of positive number", "[math]") {
+    REQUIRE(compute_sqr_root(25.0) == Approx(5.0));
+    REQUIRE(compute_sqr_root(0.0) == Approx(0.0));
+}
+
+TEST_CASE("Square root of negative number", "[math]") {
+    REQUIRE(compute_sqr_root(-1.0) == -1);
+}
