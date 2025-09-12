e9ab1e4faa3228f6b80aa1dfb3a167fc37af262d
Rabbit <166239200+RabbitAsakura@users.noreply.github.com>
2025-09-12 22:23:04 +0000

    unsorted.cpp

--- Lab2HW/unsorted.cpp
+++ Lab2HW/unsorted.cpp
@@ -1,41 +1,22 @@
 #include "unsorted.h"
 
-class UnsortedType
-{
-public:
-  UnsortedType();
-  bool IsFull() const;
-  int GetLength() const;
-  ItemType GetItem(ItemType item, bool& found);
-  bool MakeEmpty();
-  void PutItem(ItemType item);
-  void DeleteItem(ItemType item);
-  void ResetList();
-  ItemType GetNextItem();
-private:
-  int length;
-  ItemType info[MAX_ITEMS];
-  int currentPos;
-};
-
 UnsortedType::UnsortedType()
 {
   length = 0;
+  currentPos = -1;
 }
+
 bool UnsortedType::IsFull() const
 {
   return (length == MAX_ITEMS);
 }
+
 int UnsortedType::GetLength() const
 {
   return length;
 }
 
 ItemType UnsortedType::GetItem(ItemType item, bool& found) 
-// Pre:  Key member(s) of item is initialized. 
-// Post: If found, item's key matches an element's key in the 
-//       list and a copy of that element has been returned;
-//       otherwise, item is returned. 
 {
   bool moreToSearch;
   int location = 0;
@@ -58,22 +39,20 @@ ItemType UnsortedType::GetItem(ItemType item, bool& found)
   }
   return item;
 }
+
 bool UnsortedType::MakeEmpty()
-// Post: list is empty.
 {
   length = 0;
   return true;
 }
+
 void UnsortedType::PutItem(ItemType item)
-// Post: item is in the list.
 {
   info[length] = item;
   length++;
 }
+
 void UnsortedType::DeleteItem(ItemType item)
-// Pre:  item's key has been initialized.
-//       An element in the list has a key that matches item's.
-// Post: No element in the list has a key that matches item's.
 {
   int location = 0;
 
@@ -83,20 +62,14 @@ void UnsortedType::DeleteItem(ItemType item)
   info[location] = info[length - 1];
   length--;
 }
+
 void UnsortedType::ResetList()
-// Post: currentPos has been initialized.
 {
   currentPos = -1;
 }
 
 ItemType UnsortedType::GetNextItem()
-// Pre:  ResetList was called to initialized iteration.
-//       No transformer has been executed since last call.
-//       currentPos is defined.
-// Post: item is current item.
-//       Current position has been updated.
 {
   currentPos++;
   return info[currentPos];
 }
-
